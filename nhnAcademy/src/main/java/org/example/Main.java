9d365492af24d1aa3bef508563eb14428d97992f
limtaewon <tagkdj1@gmail.com>
2022-10-07 13:46:39 +0900

    final
    f

--- nhnAcademy/src/main/java/org/example/Main.java
+++ nhnAcademy/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("임태원");
         System.out.println("양현성");
         System.out.println("김상주");
         System.out.println("강명관");
